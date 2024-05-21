adc9692f8da39a0cf0ef307447749683c59f52c2
DOSalgueroM <daviniasalguero@gmail.com>
2024-05-21 19:19:03 -0400

    Agregar archivos de proyecto.

---
+++ Delegates/DelegateExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+
+{
+    delegate int MathOperation(int a, int b);
+    class DelegateExample
+    {
+        public static int Add(int a, int b)
+        {
+            int result =  a + b;
+            
+            return result;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            int result = a - b;
+            
+            return result;
+        }
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,24 @@
+﻿using Delegates;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        MathOperation mathOperation = DelegateExample.Add;
+
+        int result = mathOperation(10, 5);
+        Console.WriteLine($"Addition result = {result}");
+
+        mathOperation = DelegateExample.Subtract;
+        result = mathOperation(10, 5);
+        Console.WriteLine($"Subtraction result = {result}");
+
+
+        mathOperation += DelegateExample.Add;
+        mathOperation += DelegateExample.Subtract;
+        result = mathOperation(10, 7);
+        Console.WriteLine($"Addition and Subtraction result = {result}");
+
+    }
+}
+
